7761c0a988b2c8f460bd680ee65b1acce980c9b5
myoshioka <y.mako2@gmail.com>
2020-03-03 14:37:18 +0900

    log出力パスを追加

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+      .use(express.static(path.join(__dirname, 'public')))
+      .set('views', path.join(__dirname, 'views'))
+      .set('view engine', 'ejs')
+      .get('/', (req, res) => res.render('pages/index'))
+      .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.post('/log/', (req, res) => {
+    console.log(req.body);
+    console.log(req.headers);
+    let response = {status:'ok'};
+    res.send(response);
+});
