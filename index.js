59107f9d5863e3534718a1f1116aa0b58ac46038
myoshioka <y.mako2@gmail.com>
2020-03-03 14:58:23 +0900

    bodyをjson出力するように修正

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
