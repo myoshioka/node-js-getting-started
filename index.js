0097fcbeba2958d15c2c6167cb24f8eb24fbb609
myoshioka <y.mako2@gmail.com>
2020-03-03 14:46:12 +0900

    メイン処理を修正

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-      .use(express.static(path.join(__dirname, 'public')))
-      .set('views', path.join(__dirname, 'views'))
-      .set('view engine', 'ejs')
-      .get('/', (req, res) => res.render('pages/index'))
-      .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
 
 app.get('/log/', (req, res) => {
     console.log(req.body);
@@ -23,3 +23,5 @@ app.post('/log/', (req, res) => {
     let response = {status:'ok'};
     res.send(response);
 });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
